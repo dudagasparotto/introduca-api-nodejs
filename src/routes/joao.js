f4175381499d30ea940689a4cbebd6ef668afb0e
Joaogabriel6 <jgabriel2905@gmail.com>
2025-11-14 20:38:21 -0300

    Update joao.js

--- src/routes/joao.js
+++ src/routes/joao.js
@@ -5,19 +5,19 @@ const OnibusController = require('../controllers/usuarios');
 const PontosController = require('../controllers/produtos');
 const LocalizacaoController = require('../controllers/produtos');
 
-router.get('/onibus', OnibusController. listarOnibus);
-router.post('/onibus', OnibusController. cadastrarOnibus);
-router.patch('/onibus', OnibusController. editarOnibus);
-router.delete('/onibus', OnibusController. apagarOnibus);
+router.get('/onibus', OnibusController.listarOnibus);
+router.post('/onibus', OnibusController.cadastrarOnibus);
+router.patch('/onibus', OnibusController.editarOnibus);
+router.delete('/onibus', OnibusController.apagarOnibus);
 
-router.get('/pontos', PontosController. listarPontos);
-router.post('/pontos', PontosController. cadastrarPontos);
-router.patch('/pontos', PontosController. editarPontos);
-router.delete('/pontos', PontosController. apagarPontos);
+router.get('/pontos', PontosController.listarPontos);
+router.post('/pontos', PontosController.cadastrarPontos);
+router.patch('/pontos', PontosController.editarPontos);
+router.delete('/pontos', PontosController.apagarPontos);
 
-router.get('/localizacao', LocalizacaoController. listarLocalizacao);
-router.post('/localizacao', LocalizacaoController. cadastrarLocalizacao);
-router.patch('/localizacao', LocalizacaoController. editarLocalizacao);
-router.delete('/localizacao', LocalizacaoController. apagarLocalizacao);
+router.get('/localizacao', LocalizacaoController.listarLocalizacao);
+router.post('/localizacao', LocalizacaoController.cadastrarLocalizacao);
+router.patch('/localizacao', LocalizacaoController.editarLocalizacao);
+router.delete('/localizacao', LocalizacaoController.apagarLocalizacao);
 
 module.exports = router;
