fc70c03529237832a134cbcd4c8c2669ad6df528
KAUANI NUNES COSTA GONZAGA DE MELO <kauani.melo>
2025-11-14 20:39:38 -0300

    Create kauani.js

---
+++ src/routes/kauani.js
@@ -0,0 +1,20 @@
+const express = require('express'); 
+const router = express.Router();
+const motoristaController = require('../controllers/motorista');
+const usuariosController = require('../controllers/usuarios');
+const tiposdeUsuariosController = require('../controllers/tiposdeUsuario');
+
+router.get('/motoristas', motoristaController.listarMotorista);
+router.post('/motoristas', motoristaController.cadastrarMotorista);
+router.patch('/motoristas', motoristaController.atualizarMotorista);
+router.delete('/motoristas', motoristaController.apagarMotorista);
+
+router.get('/usuarios', usuariosController.listarUsuario);
+router.post('/usuarios', usuariosController.cadastrarUsuario);
+router.patch('/usuarios', usuariosController.atualizarUsuario);
+router.delete('/usuarios', usuariosController.apagarUsuario);
+
+router.get('/tiposdeusuarios', tiposdeUsuariosController.listarTiposdeUsuario);
+router.post('/tiposdeusuarios', tiposdeUsuariosController.cadastrarTiposdeUsuario);
+router.patch('/tiposdeusuarios', tiposdeUsuariosController.atualizarTiposdeUsuario);
+router.delete('/tiposdeusuarios', tiposdeUsuariosController.apagarTiposdeUsuario);
