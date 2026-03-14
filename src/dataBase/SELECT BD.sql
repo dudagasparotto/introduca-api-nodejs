2c308ffca7c598bd48535285bee6309089b9c4df
dudagasparotto <dudagasparotto05@gmail.com>
2026-03-13 21:18:52 -0300

    banco

---
+++ src/dataBase/SELECT BD.sql
@@ -0,0 +1,16 @@
+select * from linhas;
+select * from avaliacao;
+select * from horarios;
+select * from localizacao;
+select * from manutencao;
+select * from motorista;
+select * from onibus;
+select * from pontos;
+select * from rota_onibus;
+select * from rotas;
+select * from tipo_usuario;
+select * from usuarios;
+
+SELECT COUNT(*) AS id_motorista 
+FROM motorista; /*numero total de motoristas*/
+
