42901743f7e692ca68e1c1a8921b0fc0ab4abb2f
pedroaugruiz <pedroaugustoruiz9@gmail.com>
2026-05-06 20:39:20 -0300

    mexido

--- src/controllers/localizacao.js
+++ src/controllers/localizacao.js
@@ -1,114 +1,117 @@
-const db = require('../dataBase/connection');
+/*  PROPOSTA DE IMPLEMENTAÇÃO FUTURA  */
 
-module.exports = {
-    async listarlocalizacao (request, response) {
-        try{
 
-            const sql = `
-                SELECT 
-                    id_localizacao, id_rota_onibus, latitude_localizacao, longitude_localizacao, data_hora_localizacao
-                FROM
-                    localizacao
-                ORDER BY
-                    id_localizacao ASC;
-            `;
+// const db = require('../dataBase/connection');
 
-            const [localizacao] = await db.query(sql); 
+// module.exports = {
+//     async listarlocalizacao (request, response) {
+//         try{
 
-            return response.status(200).json(
-                {
-                sucesso: true,
-                mensagem: 'Lista de localizacao obtida com sucesso',
-                dados: localizacao
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                sucesso: false,
-                mensagem: `Erro ao listar a localizacao: ${error.message}`,
-                dados: error.message
-                }
-            );
-        }
-    },
-    async cadastrarlocalizacao (request, response) {
-        try{
+//             const sql = `
+//                 SELECT 
+//                     id_localizacao, id_rota_onibus, latitude_localizacao, longitude_localizacao, data_hora_localizacao
+//                 FROM
+//                     localizacao
+//                 ORDER BY
+//                     id_localizacao ASC;
+//             `;
 
-            const {id_da_rota_onibus, latitude_da_localizacao, longitude_da_localizacao, data_da_hora_localizacao} = request.body;
+//             const [localizacao] = await db.query(sql); 
 
-            const sql = `
-                INSERT INTO localizacao 
-                    (id_rota_onibus, latitude_localizacao, longitude_localizacao, data_hora_localizacao) 
-                VALUES
-                    (?, ?, ?, ?);
-            `;
+//             return response.status(200).json(
+//                 {
+//                 sucesso: true,
+//                 mensagem: 'Lista de localizacao obtida com sucesso',
+//                 dados: localizacao
+//                 }
+//             );
+//         } catch (error) {
+//             return response.status(500).json(
+//                 {
+//                 sucesso: false,
+//                 mensagem: `Erro ao listar a localizacao: ${error.message}`,
+//                 dados: error.message
+//                 }
+//             );
+//         }
+//     },
+//     async cadastrarlocalizacao (request, response) {
+//         try{
 
-            const values = [id_da_rota_onibus, latitude_da_localizacao, longitude_da_localizacao, data_da_hora_localizacao];
+//             const {id_da_rota_onibus, latitude_da_localizacao, longitude_da_localizacao, data_da_hora_localizacao} = request.body;
 
-            const [result] = await db.query(sql, values);
+//             const sql = `
+//                 INSERT INTO localizacao 
+//                     (id_rota_onibus, latitude_localizacao, longitude_localizacao, data_hora_localizacao) 
+//                 VALUES
+//                     (?, ?, ?, ?);
+//             `;
 
-            const dados = {
-                id: result.insertId,
-                id_da_rota_onibus, 
-                latitude_da_localizacao, 
-                longitude_da_localizacao, 
-                data_da_hora_localizacao
-            }
+//             const values = [id_da_rota_onibus, latitude_da_localizacao, longitude_da_localizacao, data_da_hora_localizacao];
 
-            return response.status(200).json(
-                {
-                sucesso: true,
-                mensagem: 'Cadastro de localizacao realizado com sucesso',
-                dados: dados
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                sucesso: false,
-                mensagem: `Erro ao cadastrar localizacao: ${error.message}`,
-                dados: error.message
-                }
-            );
-        }
-    },
-    async editarlocalizacao (request, response) {
-        try{
-            return response.status(200).json(
-                {
-                sucesso: true,
-                mensagem: 'Atualização de localizacao realizado com sucesso',
-                dados: null
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                sucesso: false,
-                mensagem: `Erro ao atualizar a localizacao: ${error.message}`,
-                dados: null
-                }
-            );
-        }
-    },
-    async apagarlocalizacao (request, response) {
-        try{
-            return response.status(200).json(
-                {
-                sucesso: true,
-                mensagem: 'Exclusão de localizacao realizada com sucesso',
-                dados: null
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                sucesso: false,
-                mensagem: `Erro ao remover localizacao: ${error.message}`,
-                dados: null
-                }
-            );
-        }
-    },
-};
+//             const [result] = await db.query(sql, values);
+
+//             const dados = {
+//                 id: result.insertId,
+//                 id_da_rota_onibus, 
+//                 latitude_da_localizacao, 
+//                 longitude_da_localizacao, 
+//                 data_da_hora_localizacao
+//             }
+
+//             return response.status(200).json(
+//                 {
+//                 sucesso: true,
+//                 mensagem: 'Cadastro de localizacao realizado com sucesso',
+//                 dados: dados
+//                 }
+//             );
+//         } catch (error) {
+//             return response.status(500).json(
+//                 {
+//                 sucesso: false,
+//                 mensagem: `Erro ao cadastrar localizacao: ${error.message}`,
+//                 dados: error.message
+//                 }
+//             );
+//         }
+//     },
+//     async editarlocalizacao (request, response) {
+//         try{
+//             return response.status(200).json(
+//                 {
+//                 sucesso: true,
+//                 mensagem: 'Atualização de localizacao realizado com sucesso',
+//                 dados: null
+//                 }
+//             );
+//         } catch (error) {
+//             return response.status(500).json(
+//                 {
+//                 sucesso: false,
+//                 mensagem: `Erro ao atualizar a localizacao: ${error.message}`,
+//                 dados: null
+//                 }
+//             );
+//         }
+//     },
+//     async apagarlocalizacao (request, response) {
+//         try{
+//             return response.status(200).json(
+//                 {
+//                 sucesso: true,
+//                 mensagem: 'Exclusão de localizacao realizada com sucesso',
+//                 dados: null
+//                 }
+//             );
+//         } catch (error) {
+//             return response.status(500).json(
+//                 {
+//                 sucesso: false,
+//                 mensagem: `Erro ao remover localizacao: ${error.message}`,
+//                 dados: null
+//                 }
+//             );
+//         }
+//     },
+// };
